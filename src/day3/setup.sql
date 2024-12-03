3f2e3340cf8a4e71541508af81e4cb8366d90382
eva <erahhov@gmail.com>
2024-12-03 20:05:06 +0100

    feat: forgot to save

--- src/day3/setup.sql
+++ src/day3/setup.sql
@@ -1,10 +1,10 @@
-DROP TABLE IF EXISTS day1_part1;
+DROP TABLE IF EXISTS day3_part1;
 
-CREATE TABLE day1_part1(
+CREATE TABLE day3_part1(
     line_number SERIAL,
     raw_data TEXT
 );
 
 
-\copy day1_part1(raw_data) FROM './data/day1' WITH (FORMAT text);
+\copy day3_part1(raw_data) FROM './data/day3' WITH (FORMAT text);
 
